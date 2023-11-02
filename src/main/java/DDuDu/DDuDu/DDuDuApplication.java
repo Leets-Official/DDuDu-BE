fb96da36c67564d315967de2214cc602bd75bbc9
rootTiket <l2007889@gmail.com>
2023-11-02 22:01:05 +0900

    init

---
+++ src/main/java/DDuDu/DDuDu/DDuDuApplication.java
@@ -0,0 +1,13 @@
+package DDuDu.DDuDu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDuDuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDuDuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DDuDu/DDuDu/DDuDuApplicationTests.java
@@ -0,0 +1,13 @@
+package DDuDu.DDuDu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDuDuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
