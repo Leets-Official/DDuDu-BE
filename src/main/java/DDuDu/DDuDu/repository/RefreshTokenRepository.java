bbabf4fc405fa4f21a7a18c203a341096a976650
rootTiket <l2007889@gmail.com>
2023-11-11 01:04:00 +0900

    feat: 리프레시 토큰 레포지토리 생성

--- src/main/java/DDuDu/DDuDu/repository/RefreshTokenRepository.java
+++ src/main/java/DDuDu/DDuDu/repository/RefreshTokenRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByUserId(Long userid);
+    
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
