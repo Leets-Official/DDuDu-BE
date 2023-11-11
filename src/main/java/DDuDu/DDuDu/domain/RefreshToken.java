f0c82f04b93d4484255b01273c7070db82b55a09
rootTiket <l2007889@gmail.com>
2023-11-11 22:34:04 +0900

    fix: 리프레시토큰의 id를 tokenId로 수정

--- src/main/java/DDuDu/DDuDu/domain/RefreshToken.java
+++ src/main/java/DDuDu/DDuDu/domain/RefreshToken.java
@@ -14,21 +14,22 @@ import lombok.NoArgsConstructor;
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false)
+    @Column(name = "tokenId", updatable = false, unique = true)
     private Long id;
 
     @Column(name = "userId", nullable = false, unique = true)
     private Long userId;
 
-    @Column(name = "refreshToken", nullable = false)
+    @Column(name = "refreshToken", nullable = false, columnDefinition = "TEXT")
     private String refreshToken;
 
     public RefreshToken(Long userId, String refreshToken) {
         this.userId = userId;
         this.refreshToken = refreshToken;
     }
-    public RefreshToken update(String newRefreshToken) {
+    public RefreshToken update(String newRefreshToken,Long userId) {
         this.refreshToken = newRefreshToken;
+        this.userId = userId;
         return this;
     }
 
