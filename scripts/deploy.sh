5ee0aea0bc65c685358a473db74a904bad619b7b
rootTiket <l20078829@gmail.com>
2023-11-14 00:11:57 +0900

    chore: 셸 스크립트 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/moduform
+REPOSITORY=/home/ubuntu/ddudu
 cd $REPOSITORY
 
-APP_NAME=moduform
+APP_NAME=ddudu
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
