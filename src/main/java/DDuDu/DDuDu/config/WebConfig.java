4ad08e0c16f9fd0f555a78096e222ab5df0a68f2
rootTiket <l20078829@gmail.com>
2023-11-14 19:22:33 +0900

    hotfix: allowCredentials를 false로 수정

--- src/main/java/DDuDu/DDuDu/config/WebConfig.java
+++ src/main/java/DDuDu/DDuDu/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Authorization-refresh")
                 .maxAge(3600);
