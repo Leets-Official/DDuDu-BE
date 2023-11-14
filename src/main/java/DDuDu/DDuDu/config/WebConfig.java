a6daedcac8e83195833ee9b6347b35ffb78a103f
rootTiket <l20078829@gmail.com>
2023-11-14 19:00:39 +0900

    feat: WebConfig 생성

---
+++ src/main/java/DDuDu/DDuDu/config/WebConfig.java
@@ -0,0 +1,19 @@
+package DDuDu.DDuDu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Authorization-refresh")
+                .maxAge(3600);
+    }
+}
