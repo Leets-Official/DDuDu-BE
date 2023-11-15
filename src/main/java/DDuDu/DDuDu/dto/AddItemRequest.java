d3295147274a0bd08f026688c671bb8007285918
노정완 <jeongwan.noh@gmail.com>
2023-11-16 02:01:44 +0900

    feat: 투두가 생성될 때 자동으로 생성 날짜를 DB에 저장하는 기능 추가

--- src/main/java/DDuDu/DDuDu/dto/AddItemRequest.java
+++ src/main/java/DDuDu/DDuDu/dto/AddItemRequest.java
@@ -1,22 +1,26 @@
 package DDuDu.DDuDu.dto;
 
 import DDuDu.DDuDu.domain.Item;
+import DDuDu.DDuDu.domain.User;
+import DDuDu.DDuDu.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class AddItemRequest {
-
+    private UserRepository userRepository;
     private String content;
-    private Date createDate;
+    private LocalDateTime createDate;
     private Date deadline;
     private Boolean isCompleted;
     private Boolean isRoutine;
+    private User user;
 
     public Item toEntity() {
         return Item.builder()
@@ -25,6 +29,11 @@ public class AddItemRequest {
                 .deadline(deadline)
                 .isCompleted(isCompleted)
                 .isRoutine(isRoutine)
+                .user(user)
                 .build();
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
