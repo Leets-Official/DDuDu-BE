da599d7492065e71a92cf73033c320d6aca7592c
rootTiket <l20078829@gmail.com>
2023-11-16 16:08:54 +0900

    Hotfix: cors 오타수정

--- src/main/java/DDuDu/DDuDu/config/WebConfig.java
+++ src/main/java/DDuDu/DDuDu/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://http://3.24.110.92:8000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.24.110.92:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false)
                 .allowedHeaders("*")
